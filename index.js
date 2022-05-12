c761d3a87fff18d0f5a179b01ec984554b22a10f
mcrsrvcs <mcrsrvcs@gmail.com>
2022-05-12 09:47:29 +0530

    this

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/',(req,res)=>{
     let data = [
         {
